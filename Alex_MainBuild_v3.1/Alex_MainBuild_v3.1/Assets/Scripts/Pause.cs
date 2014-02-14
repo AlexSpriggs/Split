ce25ad66b8e4bdf46bb65cd0fd41fe3dba6ed022
Zerophase <mikelojkovic@gmail.com>
2014-02-14 13:12:34 -0600

    Made Cameras change to orthographic when paused. It gives weird graphical results for the background. Should we stick with perspective?
    Also added a check for the UnityPlayerController.  It operates a bit differently, so I had to disable the MouseLook.cs script when paused.

--- Alex_MainBuild_v3.1/Alex_MainBuild_v3.1/Assets/Scripts/Pause.cs
+++ Alex_MainBuild_v3.1/Alex_MainBuild_v3.1/Assets/Scripts/Pause.cs
@@ -10,6 +10,15 @@ public class Pause : MonoBehaviour
 	Color guiColor = Color.white;
 	Color pauseBox = Color.white;
 
+	private List<GameObject> unityPlayerController = new List<GameObject>();
+
+	private List<Camera> cameras = new List<Camera>();
+
+	private GameObject[] camerasToAdd = new GameObject[2];
+	private bool testCameras = true;
+
+	private float x;
+	private float y;
 //	public List<GameObject> gameobjects = new List<GameObject>();
 //	private List<MonoBehaviour> scripts = new List<MonoBehaviour>();
 	// Use this for initialization
@@ -30,20 +39,65 @@ public class Pause : MonoBehaviour
 		guiColor.a = .5f;
 		pauseBox.a = .75f;
 		paused = (Texture)Resources.Load("Paused");
+
+		if (GameObject.FindGameObjectWithTag("Player").GetComponent<MouseLook>()) 
+		{
+			unityPlayerController.Add(GameObject.FindGameObjectWithTag("Player"));
+			unityPlayerController.Add(GameObject.FindGameObjectWithTag("MainCamera"));
+			unityPlayerController.Add(GameObject.FindGameObjectWithTag("MainCameraTwo"));
+		}
+			
+
+		camerasToAdd = GameObject.FindGameObjectsWithTag("MainCamera");
+		for (int i = 0; i < camerasToAdd.Length; i++) 
+		{
+			cameras.Add(camerasToAdd[i].GetComponent<Camera>());
+		}
+
+		Resolution.Instance.CorrectResolution();
+		x = Resolution.Instance.X;
+		y = Resolution.Instance.Y;
+
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
+		if (testCameras) 
+		{
+			Debug.Log("Cameras in cameras: " + cameras.Count);
+			testCameras = false;
+		}
+
 		if(gamePaused && Input.GetKeyDown(KeyCode.Escape))
 		{
+			deactivateUnityMouseLook (true);
+
+			changeCameraType (false);
 			Time.timeScale = 1.0f;
 			gamePaused = false;
 		}
 		else if (!gamePaused && Input.GetKeyDown(KeyCode.Escape)) 
 		{
+			deactivateUnityMouseLook (false);
+
+			changeCameraType (true);
 			gamePaused = true;
 			Time.timeScale = 0.0f;
+		}
+	}
+
+	void deactivateUnityMouseLook (bool enable)
+	{
+		if (unityPlayerController.Count > 0)
+			foreach (GameObject controller in unityPlayerController)
+				controller.GetComponent<MouseLook>().enabled = enable;
+	}
+
+	void changeCameraType (bool ortho)
+	{
+		foreach (Camera cameraElement in cameras) {
+			cameraElement.orthographic = ortho;
 		}
 	}
 
@@ -66,7 +120,7 @@ public class Pause : MonoBehaviour
 			GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), paused, ScaleMode.StretchToFill, true, 0);
 
 			GUI.color = pauseBox;
-			GUI.DrawTexture(new Rect(200, 200, 1000, 500), paused, ScaleMode.StretchToFill);
+			GUI.DrawTexture(new Rect( x * 100, y * 50, x * 750, y * 500), paused, ScaleMode.StretchToFill);
 		}
 	}
 }
