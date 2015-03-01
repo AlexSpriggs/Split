4fe1d765b32d4bbf798cd16f8572de4b568596c4
zerophase <mikelojkovic@gmail.com>
2015-02-28 19:18:48 -0600

    Split 2.0

---
+++ Split/Assets/KeyManagerScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeyManagerScript : MonoBehaviour {
+
+	public KeyCode forward;
+	public KeyCode back;
+	public KeyCode right;
+	public KeyCode left;
+	public KeyCode jump;
+
+	public KeyCode p2forward;
+	public KeyCode p2back;
+	public KeyCode p2right;
+	public KeyCode p2left;
+	public KeyCode p2jump;
+
+	public KeyCode camRotRight;
+	public KeyCode camRotLeft;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Split/Assets/ZoneChange.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZoneChange : MonoBehaviour {
+
+	RiftManagerScript RMS;
+
+	public enum rifts{old, knew};
+//	public string ZoneInfo;
+
+	public rifts currentRift;
+
+	// Use this for initialization
+	void Start () {
+		RMS = GameObject.FindGameObjectWithTag("RiftManager").GetComponent<RiftManagerScript>();
+//		if(ZoneInfo == "old")
+//			currentRift = rifts.old;
+//		else if(ZoneInfo == "knew")
+//			currentRift = rifts.knew;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//fix, it works but not in the right way
+
+		if(currentRift == rifts.knew){
+			if(RMS.currentRift == RiftManagerScript.rifts.knew){
+				this.gameObject.renderer.enabled = false;
+				this.gameObject.collider.enabled = false;
+			}
+			else{
+				this.gameObject.renderer.enabled = true;
+				this.gameObject.collider.enabled = true;
+			}
+		}
+
+		if(currentRift == rifts.old){
+			if(RMS.currentRift == RiftManagerScript.rifts.old){
+				this.gameObject.renderer.enabled = false;
+				this.gameObject.collider.enabled = false;
+			}else{
+				this.gameObject.renderer.enabled = true;
+				this.gameObject.collider.enabled = true;
+			}
+		}
+
+//		if(RMS.currentRift == RiftManagerScript.rifts.knew && currentRift == rifts.knew){
+//			print("RMS new, CR new");
+//			this.gameObject.SetActive(false);
+//		}
+//		else if (RMS.currentRift == RiftManagerScript.rifts.old && currentRift == rifts.knew){
+//			print("RMS old, CR new");
+//			this.gameObject.SetActive(true);
+//		}
+//
+//		else if(RMS.currentRift == RiftManagerScript.rifts.old && currentRift == rifts.old){
+//			print("RMS old, CR old");
+//			this.gameObject.SetActive(false);
+//		}
+//		else if (RMS.currentRift == RiftManagerScript.rifts.knew && currentRift == rifts.old){
+//			print("RMS new, CR old");
+//			this.gameObject.SetActive(true);
+//		}
+	
+	}
+
+	void OnTriggerEnter(Collider other){
+		if(RMS.currentRift == RiftManagerScript.rifts.knew && currentRift == rifts.old){
+			RMS.currentRift = RiftManagerScript.rifts.old;
+		}else if(RMS.currentRift == RiftManagerScript.rifts.old && currentRift == rifts.knew){
+			RMS.currentRift = RiftManagerScript.rifts.knew;
+		}
+	}
+}
